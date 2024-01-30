64dfc9b91a6b9e5573a86d381bf22282e3e90b24
oussamaef06 <oussamaelfarihi@gmail.com>
2024-01-30 15:40:11 +0100

    adding product to db

--- e-commerce/app/Http/Controllers/ProductController.php
+++ e-commerce/app/Http/Controllers/ProductController.php
@@ -4,7 +4,30 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Product;
+
 class ProductController extends Controller
 {
-    //
+    public function index(){
+        return view('products.index');
+    }
+
+    public function create(){
+        return view('products.create');
+    }
+
+    public function store(Request $request) {
+        // dd($request);
+        $data = $request->validate([
+            'name' => 'required',
+            'qty' => 'required|numeric',
+            'price' => 'required|numeric',
+            'description' => 'nullable',
+        ]);
+    
+        $newProduct = Product::create($data);
+    
+        return redirect(route('product.index'));
+    }
+    
 }
--- e-commerce/app/Models/Product.php
+++ e-commerce/app/Models/Product.php
@@ -1,18 +1,13 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Product extends Model
-{
+class Product extends Model {
     use HasFactory;
-    
-    protected $fillabel = [
-        'name',
-        'qty',
-        'price',
-        'description'
+
+    protected $fillable = [
+        'name', 'qty', 'price', 'description'
     ];
 }
